990b1e0044292fc2ed77342953bce2ebf478d738
Huan183 <rachelqiu183@yahoo.com>
2018-09-23 22:23:54 +0800

    bouncing is dead

--- js/libs/Scroller.js
+++ js/libs/Scroller.js
@@ -6,7 +6,7 @@ key：
 -4- 要enable滑动指定target到某个位置的话，invoke <instance>.scrollTo(<number>) 参数指定位置的px值
 
 todo:
-增加boucing的效果
+bouncing写错，再来
 */
 
 export default class Scroller {
@@ -17,6 +17,12 @@ export default class Scroller {
     this.direction = direction;
 
     // shared
+    this.game = targetToScroll.game;
+    this.tween = this.game.add.tween(targetToScroll);
+    this.axis = direction === 'vertical' ? 'y' : 'x';
+    // this.bounceDistance = 0;
+    // this.bouncing = false;
+
     this.lastOffset = 0;
     this.offset = 0;
     this.target = 0;
@@ -33,27 +39,39 @@ export default class Scroller {
       MARGIN: 100, // increase the value of max
       FACTOR: 0.7, // factor 0.6 is tweakable, fancy a 'heavy-scrolling-feel', reduce the value of factor.
       DELTA_STOP_THRESHOLD: 0.5, // when to stop auto-scrolling after release pointer
-      DELTA_START_THRESHOLD: 2, // when to start scrolling when dragging
+      DELTA_START_THRESHOLD: 5, // when to start scrolling when dragging
+      TWEEN_DURATION: 400,
+      // BOUNCE_DURATION: 200,
+      // BOUNCE_THRESHOLD: 4,
+      // BOUNCE_MARGIN: 105
     };
   }
 
-  touchScreen = (pointerGame, pointerObj) => {
+  _touchScreen = (pointerGame, pointerObj) => {
+    // if tweening is running, stop it
+    if (this.tween.isRunning) {
+      this.tween.stop(true);
+    }
     let pointer = this.targetToScroll ? pointerObj : pointerGame;
-    let initPos = this.direction === 'vertical' ? pointer.y : pointer.x;
-    this.initializeDataBehavior(initPos);
+    let initPos = pointer[this.axis];
+    console.log(`initPos: ${initPos}`);
+    this.game.input.addMoveCallback(this._dragOnScreen);
+    this._initializeDataBehavior(initPos);
     return false;
   };
 
-  initializeDataBehavior = initPos => {
+  _initializeDataBehavior = initPos => {
     let targetSize, minosSize;
     clearInterval(this.ticker);
+    // this.bounceDistance = 0;
+    this.bouncing = false;
     this.pressed = true;
-    this.lastY = initPos;
+    this.lastY = initPos || 0;
 
     this.lastOffset = this.offset;
     this.vUsed = this.amplitude = 0;
     this.timestamp = Date.now();
-    this.ticker = setInterval(this.trackVelocity, 100);
+    this.ticker = setInterval(this._trackVelocity, 100);
 
     this.min = 0;
     targetSize =
@@ -75,7 +93,7 @@ export default class Scroller {
     return true;
   };
 
-  trackVelocity = () => {
+  _trackVelocity = () => {
     // trace and update vUsed every 100ms
     let now, elapsed, vReal, delta;
     if (!this.pressed) return false;
@@ -90,36 +108,86 @@ export default class Scroller {
     this.vUsed = 0.8 * vReal + 0.2 * this.vUsed;
   };
 
-  dragOnScreen = pointer => {
+  _dragOnScreen = pointer => {
+    // dont get in the way when tweening, not really works when the arrows are clicked, as this one invokes faster
+    if (this.tween.isRunning) return false;
     let nowY, delta;
     if (this.pressed) {
-      nowY = this.direction === 'vertical' ? pointer.y : pointer.x;
+      nowY = pointer[this.axis];
       delta = this.lastY - nowY; // lastY is the reference
+      // this.bounceDistance += delta;
       if (delta > this.config.DELTA_START_THRESHOLD || delta < -this.config.DELTA_START_THRESHOLD) {
+        // console.log(`drag delta: ${this.lastY} - ${nowY} = delta`);
+        if (this.bouncing) return false;
         this.lastY = nowY;
-        this.getScrolling(delta + this.offset);
+        this._getScrolling(delta + this.offset);
       }
     }
     return false;
   };
 
-  getScrolling = delta2scroll => {
+  // not good
+  _getBouncing = (bounceDistance) => {
+    // `max` and `min` are the pos(x || y) of this.targetToScroll's extremities at this.axis
+    let max = this.direction === 'vertical'
+      ? -(this.targetToScroll.height - this.targetToScroll.game.camera.view.height)
+      : -(this.targetToScroll.width - this.targetToScroll.game.camera.view.width);
+    let min = this.min;
+    let pos = this.targetToScroll[this.axis];
+    if (pos > max && pos < min) return false; // pos is ranging from [-0, -this.targetToScroll.height]
+
+    let end = pos === max ? max : min;
+    if (this.pressed) {
+      if (this.tween.isRunning === false) {
+        this.tween = this.game.add.tween(this.targetToScroll);
+        this.tween.to(
+          {
+            [this.axis]: -bounceDistance
+          },
+          this.config.BOUNCE_DURATION,
+          Phaser.Easing.Quadratic.In,
+          true,
+          0,
+          0,
+          false
+        );
+      }
+
+    } else if (!this.pressed) {
+      console.log('bounce back');
+        this.tween = this.game.add.tween(this.targetToScroll);
+        this.tween.to(
+          {
+            [this.axis]: end
+          },
+          this.config.BOUNCE_DURATION,
+          Phaser.Easing.Quadratic.Out,
+          true,
+          0,
+          0,
+          false
+        );
+    }
+    return false;
+  };
+
+
+  _getScrolling = delta2scroll => {
+    if (this.bouncing) return false;
     this.offset =
       delta2scroll > this.max
         ? this.max
         : delta2scroll < this.min
           ? this.min
           : delta2scroll;
-    if (this.direction === 'vertical') {
-      this.targetToScroll.y = -this.offset;
-    } else {
-      this.targetToScroll.x = -this.offset;
-    }
+
+    this.targetToScroll[this.axis] = -this.offset;
   };
 
-  releasePointer = () => {
+  _releasePointer = () => {
     clearInterval(this.ticker);
     this.pressed = false;
+    this.game.input.deleteMoveCallback(this._dragOnScreen);
 
     if (this.vUsed > this.config.AUTO_SCROLL_THRESHOLD || this.vUsed < -this.config.AUTO_SCROLL_THRESHOLD) {
 
@@ -127,12 +195,13 @@ export default class Scroller {
       this.target = Math.round(this.offset + this.amplitude);
       this.timestamp = Date.now();
       // requestAnimationFrame only schedules a single update to the script-based animation. If subsequent animation frames are needed, then requestAnimationFrame will need to be called again from within the callback.
-      requestAnimationFrame(this.getAutoSlide);
+      requestAnimationFrame(this._getAutoSlide);
     }
+
     return false;
   };
 
-  getAutoSlide = () => {
+  _getAutoSlide = () => {
     let timeConstant = 325;
     let elapsed, delta;
 
@@ -140,19 +209,20 @@ export default class Scroller {
       elapsed = Date.now() - this.timestamp;
       delta = -this.amplitude * Math.exp(-elapsed / timeConstant);
       if (delta > this.config.DELTA_STOP_THRESHOLD || delta < -this.config.DELTA_STOP_THRESHOLD) {
-        this.getScrolling(this.target + delta);
-        requestAnimationFrame(this.getAutoSlide);
+        this._getScrolling(this.target + delta);
+        requestAnimationFrame(this._getAutoSlide);
       } else {
-        this.getScrolling(this.target);
+        this._getScrolling(this.target);
       }
     }
   };
 
+
+
   enableScroll = () => {
-    this.targetToScroll.game.input.addMoveCallback(this.dragOnScreen);
     this.targetToScroll.setAllChildren('inputEnabled', true);
-    this.targetToScroll.onChildInputDown.add(this.touchScreen);
-    this.targetToScroll.onChildInputUp.add(this.releasePointer);
+    this.targetToScroll.onChildInputDown.add(this._touchScreen);
+    this.targetToScroll.onChildInputUp.add(this._releasePointer);
   };
 
   scrollToTop = () => {
@@ -160,7 +230,7 @@ export default class Scroller {
   };
 
   scrollTo = pos => {
-    // stop the manual scrolling
+    // resolve the final pos from its valid range, assign to posSettled
     let posSettled =
       this.direction === 'vertical'
         ? pos > this.targetToScroll.height
@@ -169,22 +239,27 @@ export default class Scroller {
         : pos > this.targetToScroll.width
           ? this.targetToScroll.width
           : pos;
-    let axis = this.direction === 'vertical' ? 'y' : 'x';
 
+    // level up this.offset and this.lastOffset, and init important value
     this.offset = posSettled;
-    this.initializeDataBehavior();
+    this.lastOffset = this.offset;
+    this.vUsed = this.amplitude = 0;
 
     // do the tweening
-    let game = this.targetToScroll.game;
-    let tween = game.add.tween(this.targetToScroll);
-    tween.to(
-      {
-        [axis]: -pos
-      },
-      500,
-      Phaser.Easing.Default,
-      true,
-      0
-    );
+    if (!this.tween || this.tween.isRunning === false) {
+      console.log('tweening start');
+      this.tween = this.game.add.tween(this.targetToScroll);
+      this.tween.to(
+        {
+          [this.axis]: Number(posSettled) * (-1)
+        },
+        this.config.TWEEN_DURATION,
+        Phaser.Easing.Default,
+        true,
+        0,
+        0,
+        false
+      );
+    }
   };
 }
