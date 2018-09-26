fb6372d6f685307b582097c3cbb1d47799b9b81d
Huan183 <rachelqiu183@yahoo.com>
2018-09-26 11:14:15 +0800

    try to lift up the contentGroup for Modal

--- components/ModalLevel.js
+++ components/ModalLevel.js
@@ -1,46 +1,143 @@
 import ModalRaw from './ModalRaw.js';
+import UpgradeItem from './UpgradeItem.js';
+import PanelUpgrade from './PanelUpgrade.js';
 
 window.PIXI = require('../js/libs/pixi.min');
 window.p2 = require('../js/libs/p2.min');
 window.Phaser = require('../js/libs/phaser-split.min');
 
-// let const = {};
-function fn () {
-  console.log('lala');
+const LEVEL = {
+  aWidth: 537,
+  aHeight: 178,
+  desHeight: 85
+};
+
+
+function getFontStyle (fSize, color) {
+  return {
+    fontWeight: 'bold',
+    fontSize: fSize,
+    fill: color || '#3A0A00', // '#00FF00',
+    boundsAlignH: 'center',
+    boundsAlignV: 'middle',
+  };
 }
 
+function getContextGroupInit() {
+    // 添加的东西 y 要 >= this.headingH
+    const OFFSET = this.headingH;
+    this.avatarBg = this.game.make.graphics((this.w - LEVEL.aWidth) / 2, OFFSET); // graphics( [x] [, y] )
+    this.avatarBg.beginFill(0x000000, 0.1);
+    this.avatarBg.drawRect(0, 0, LEVEL.aWidth, LEVEL.aHeight);
+    this.avatarBg.endFill();
+
+    this.avatarGroup = this.game.add.group(this.avatarBg);
+    this.avatar = this.game.make.image(40, 110, 'avatar_tran');
+    this.avatarHeading = this.game.make.text(60 + this.avatar.width, 120, '下一次大升级', getFontStyle('24px'));
+    this.avatarDesBg = this.game.make.graphics(0, 0);
+    this.avatarDesBg.beginFill(0x000000, 0.1);
+    this.avatarDesBg.drawRect(0, 0, 339, 30);
+    this.avatarDesBg.endFill();
+    this.avatarDesBg.alignTo(this.avatarHeading, Phaser.BOTTOM_LEFT, 0, 10);
+    this.avatarDesTxt = this.game.make.text(0, 0, '将在等级333时获得额外的运输工人', getFontStyle('18px', 'white'));
+    this.avatarDesTxt.setTextBounds(0, 0, 339, 30); // 同上
+    this.avatarDesTxt.alignTo(this.avatarDesBg, Phaser.Phaser.TOP_LEFT, 0, -28);
+
+    this.avatarBar = this.game.make.graphics(0, 0);
+    this.avatarBar.beginFill(0x3A0A00, 0.8);
+    this.avatarBar.drawRect(0, 0, 339, 30);
+    this.avatarBar.endFill();
+    this.avatarBar.alignTo(this.avatarHeading, Phaser.BOTTOM_LEFT, 0, 70);
+
+    this.avatarBarGained = this.game.make.graphics(0, 0);
+    this.avatarBarGained.beginFill(0x38ec43, 1);
+    this.avatarBarGained.drawRect(0, 0, 139, 30);
+    this.avatarBarGained.endFill();
+    this.avatarBarGained.alignTo(this.avatarHeading, Phaser.BOTTOM_LEFT, 0, 70);
+
+    this.avatarArrow = this.game.make.image(0, 0, 'arrow_levelUp');
+    this.avatarArrow.alignIn(this.avatarBarGained, Phaser.BOTTOM_LEFT, 10);
+
+    this.avatarGroup.addChild(this.avatar);
+    this.avatarGroup.addChild(this.avatarHeading);
+    this.avatarGroup.addChild(this.avatarDesBg);
+    this.avatarGroup.addChild(this.avatarDesTxt);
+    this.avatarGroup.addChild(this.avatarBar);
+    this.avatarGroup.addChild(this.avatarBarGained);
+    this.avatarGroup.addChild(this.avatarArrow);
+
+    this.desGroup = this.game.add.group();
+    // gap 16
+    let item1 = new UpgradeItem({
+      game: this.game,
+      parent: this.contentGroup,
+      key: 'icon_max_resource',
+      txt: '已运输最大资源',
+      x: (this.w - LEVEL.aWidth) / 2,
+      y: 290,
+      currTxt: '58aa/分',
+      futureTxt: '+55aa/分'
+    });
+    let item2 = new UpgradeItem({
+      game: this.game,
+      parent: this.contentGroup,
+      key: 'icon_transporter',
+      txt: '运输工',
+      x: (this.w - LEVEL.aWidth) / 2,
+      y: 290 + 85 + 17,
+      currTxt: '9',
+      futureTxt: '+0'
+    });
+
+    let item3 = new UpgradeItem({
+      game: this.game,
+      parent: this.contentGroup,
+      key: 'icon_transporter_capacity',
+      txt: '运输工能力',
+      x: (this.w - LEVEL.aWidth) / 2,
+      y: 290 + 85 * 2 + 17 * 2,
+      currTxt: '58aa',
+      futureTxt: '+55ac'
+    });
+
+    let item4 = new UpgradeItem({
+      game: this.game,
+      parent: this.contentGroup,
+      key: 'icon_loading_speed',
+      txt: '运输工载运能力',
+      x: (this.w - LEVEL.aWidth) / 2,
+      y: 290 + 85 * 3 + 17 * 3,
+      currTxt: '18.4aa/秒',
+      futureTxt: '+55aa/秒'
+    });
+
+    let item5 = new UpgradeItem({
+      game: this.game,
+      parent: this.contentGroup,
+      key: 'icon_walk_speed',
+      txt: '运输工行走速度',
+      x: (this.w - LEVEL.aWidth) / 2,
+      y: 290 + 85 * 4 + 17 * 4,
+      currTxt: '0.10米/分',
+      futureTxt: '+0.01米/分'
+    });
+
+    let upgradeBtns = new PanelUpgrade({
+      game: this.game,
+      parent: this.contentGroup
+    });
+
+    this.contentGroup.addChild(this.avatarBg);
+    this.contentGroup.addChild(this.avatarGroup);
+  }
+
+
 class ModalLevel extends ModalRaw {
-  constructor({game, headingTxt, scrollable, cb=fn}) {
+  constructor({game, headingTxt, scrollable, cb = getContextGroupInit }) {
     // parems
     super(game, headingTxt, undefined, undefined, scrollable, cb);
-    this.data = 'data';
-    // this.test = this._test;
 
-    this.test();
-  }
-
-  _handleUpgrade = () => {
-    console.log('可以。。。');
-  }
-
-  getContextGroupInit = () => {
-    // 添加的东西 y 要 >= this.headingH
-    console.log('执行？');
-    this.test = this.game.make.graphics(0, 0); // graphics( [x] [, y] )
-    this.test.beginFill(0xFF0000);
-    this.test.drawRect(80, this.headingH + 20, this.w - 160, this.h);
-    this.test.endFill();
-    this.test1 = this.game.make.graphics(0, 0); // graphics( [x] [, y] )
-    this.test1.beginFill(0x00FF00);
-    this.test1.drawRect(80, this.headingH + 20 + this.h, this.w - 160, this.h);
-    this.test1.endFill();
-    this.test2 = this.game.make.graphics(0, 0); // graphics( [x] [, y] )
-    this.test2.beginFill(0x0000FF);
-    this.test2.drawRect(80, this.headingH + 20 + this.h * 2, this.w - 160, this.h);
-    this.test2.endFill();
-    this.contentGroup.addChild(this.test);
-    this.contentGroup.addChild(this.test1);
-    this.contentGroup.addChild(this.test2);
+    this.test = () => {console.log('child');};
   }
 }
 
--- components/ModalRaw.js
+++ components/ModalRaw.js
@@ -1,9 +1,4 @@
 import Scroller from './Scroller.js';
-import UpgradeItem from './UpgradeItem.js';
-import PanelUpgrade from './PanelUpgrade.js';
-// import range from '../js/libs/_/range';
-
-
 window.PIXI = require('../js/libs/pixi.min');
 window.p2 = require('../js/libs/p2.min');
 window.Phaser = require('../js/libs/phaser-split.min');
@@ -42,22 +37,6 @@ const FONT_STYLE = {
   boundsAlignV: 'middle',
 };
 
-function getFontStyle (fSize, color) {
-  return {
-    fontWeight: 'bold',
-    fontSize: fSize,
-    fill: color || '#3A0A00', // '#00FF00',
-    boundsAlignH: 'center',
-    boundsAlignV: 'middle',
-  };
-}
-
-const LEVEL = {
-  aWidth: 537,
-  aHeight: 178,
-  desHeight: 85
-};
-
 class ModalRaw extends window.Phaser.Group {
   // new Group(game [, parent] [, name] [, addToStage] [, enableBody] [, physicsBodyType])
   constructor(game, headingTxt, height = config.HEIGHT, width = config.WIDTH, scrollable = true, cb, priority = 1000, headingH = 100) {
@@ -78,16 +57,12 @@ class ModalRaw extends window.Phaser.Group {
     this.scroller = null;
     this.x = 0;
     this.y = 0;
-    this.test = cb || this._test;
-    // this.test = this._test;
+
+    // 方法
+    this.getContextGroupInit = cb.bind(this) || null;
 
     // init
     this._getInit();
-    this.test();
-  }
-
-  _test = () => {
-    console.log('parent');
   }
 
   _getInit = () => {
@@ -110,14 +85,15 @@ class ModalRaw extends window.Phaser.Group {
     this.setAllChildren('input.priorityID', this.priorityID);
     this.btnClose.input.priorityID = this.priorityID + 1;
 
-    // prep for scrolling
+    // prep for scrolling, let the inherit class call this instead
     this._getScrollWhenNeeded();
   }
 
   _getSubGroupInit = () => {
-    // frame -> sub-group
+    /* frame, heading, btn_close, mask for contentGroup */
     // this._setMask4SubGroup();
 
+    // frame
     this.frame = this.game.make.graphics(0, 0); // graphics( [x] [, y] )
     this.frame.beginFill(config.FRAME_RGB);
     this.frame.drawRect(0, 0, this.w, this.h);
@@ -145,9 +121,6 @@ class ModalRaw extends window.Phaser.Group {
     this.subGroup.addChild(this.contentGroup);
     this.subGroup.addChild(this.btnClose);
     this.subGroup.addChild(this.heading);
-
-
-
   }
 
   _createVeil = () => {
@@ -167,6 +140,7 @@ class ModalRaw extends window.Phaser.Group {
     this.veil.events.onInputDown.add(this._handleClose);
   }
 
+  // for improve, not use yet
   _createVeilTop = () => {
     let x = -this.x;
     let y = -this.y;
@@ -214,113 +188,9 @@ class ModalRaw extends window.Phaser.Group {
 
   // try-out 在继承的类那里直接调用这个方法来添加内容
   getContextGroupInit = () => {
-    // 添加的东西 y 要 >= this.headingH
-    const OFFSET = this.headingH;
-    this.avatarBg = this.game.make.graphics((this.w - LEVEL.aWidth) / 2, OFFSET); // graphics( [x] [, y] )
-    this.avatarBg.beginFill(0x000000, 0.1);
-    this.avatarBg.drawRect(0, 0, LEVEL.aWidth, LEVEL.aHeight);
-    this.avatarBg.endFill();
-
-    this.avatarGroup = this.game.add.group(this.avatarBg);
-    this.avatar = this.game.make.image(40, 110, 'avatar_tran');
-    this.avatarHeading = this.game.make.text(60 + this.avatar.width, 120, '下一次大升级', getFontStyle('24px'));
-    this.avatarDesBg = this.game.make.graphics(0, 0);
-    this.avatarDesBg.beginFill(0x000000, 0.1);
-    this.avatarDesBg.drawRect(0, 0, 339, 30);
-    this.avatarDesBg.endFill();
-    this.avatarDesBg.alignTo(this.avatarHeading, Phaser.BOTTOM_LEFT, 0, 10);
-    this.avatarDesTxt = this.game.make.text(0, 0, '将在等级333时获得额外的运输工人', getFontStyle('18px', 'white'));
-    this.avatarDesTxt.setTextBounds(0, 0, 339, 30); // 同上
-    this.avatarDesTxt.alignTo(this.avatarDesBg, Phaser.Phaser.TOP_LEFT, 0, -28);
-
-    this.avatarBar = this.game.make.graphics(0, 0);
-    this.avatarBar.beginFill(0x3A0A00, 0.8);
-    this.avatarBar.drawRect(0, 0, 339, 30);
-    this.avatarBar.endFill();
-    this.avatarBar.alignTo(this.avatarHeading, Phaser.BOTTOM_LEFT, 0, 70);
-
-    this.avatarBarGained = this.game.make.graphics(0, 0);
-    this.avatarBarGained.beginFill(0x38ec43, 1);
-    this.avatarBarGained.drawRect(0, 0, 139, 30);
-    this.avatarBarGained.endFill();
-    this.avatarBarGained.alignTo(this.avatarHeading, Phaser.BOTTOM_LEFT, 0, 70);
-
-    this.avatarArrow = this.game.make.image(0, 0, 'arrow_levelUp');
-    this.avatarArrow.alignIn(this.avatarBarGained, Phaser.BOTTOM_LEFT, 10);
-
-    this.avatarGroup.addChild(this.avatar);
-    this.avatarGroup.addChild(this.avatarHeading);
-    this.avatarGroup.addChild(this.avatarDesBg);
-    this.avatarGroup.addChild(this.avatarDesTxt);
-    this.avatarGroup.addChild(this.avatarBar);
-    this.avatarGroup.addChild(this.avatarBarGained);
-    this.avatarGroup.addChild(this.avatarArrow);
-
-    this.desGroup = this.game.add.group();
-    // gap 16
-    let item1 = new UpgradeItem({
-      game: this.game,
-      parent: this.contentGroup,
-      key: 'icon_max_resource',
-      txt: '已运输最大资源',
-      x: (this.w - LEVEL.aWidth) / 2,
-      y: 290,
-      currTxt: '58aa/分',
-      futureTxt: '+55aa/分'
-    });
-    let item2 = new UpgradeItem({
-      game: this.game,
-      parent: this.contentGroup,
-      key: 'icon_transporter',
-      txt: '运输工',
-      x: (this.w - LEVEL.aWidth) / 2,
-      y: 290 + 85 + 17,
-      currTxt: '9',
-      futureTxt: '+0'
-    });
-
-    let item3 = new UpgradeItem({
-      game: this.game,
-      parent: this.contentGroup,
-      key: 'icon_transporter_capacity',
-      txt: '运输工能力',
-      x: (this.w - LEVEL.aWidth) / 2,
-      y: 290 + 85 * 2 + 17 * 2,
-      currTxt: '58aa',
-      futureTxt: '+55ac'
-    });
-
-    let item4 = new UpgradeItem({
-      game: this.game,
-      parent: this.contentGroup,
-      key: 'icon_loading_speed',
-      txt: '运输工载运能力',
-      x: (this.w - LEVEL.aWidth) / 2,
-      y: 290 + 85 * 3 + 17 * 3,
-      currTxt: '18.4aa/秒',
-      futureTxt: '+55aa/秒'
-    });
-
-    let item5 = new UpgradeItem({
-      game: this.game,
-      parent: this.contentGroup,
-      key: 'icon_walk_speed',
-      txt: '运输工行走速度',
-      x: (this.w - LEVEL.aWidth) / 2,
-      y: 290 + 85 * 4 + 17 * 4,
-      currTxt: '0.10米/分',
-      futureTxt: '+0.01米/分'
-    });
-
-    let upgradeBtns = new PanelUpgrade({
-      game: this.game,
-      parent: this.contentGroup
-    });
-
-    this.contentGroup.addChild(this.avatarBg);
-    this.contentGroup.addChild(this.avatarGroup);
-
+    // empty
   }
+
 
 }
 
